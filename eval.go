2d0282c3dba35e32efa8c4ff3bd15cec669c9012
zhuyingjie <m6567fc@outlook.com>
2018-05-22 11:27:41 +0800

    增加规则集判断方法

--- eval.go
+++ eval.go
@@ -2,10 +2,10 @@ package rule
 
 import (
 	"errors"
-	"strconv"
 	"regexp"
+	"strconv"
 	"strings"
-)	
+)
 
 var commentPattern = regexp.MustCompile(`/\*.*?\*/`)
 
@@ -14,7 +14,7 @@ func eval(node *astNode, e *env) (Ruler, error) {
 	case andToken:
 		if node.next == nil {
 			return nil, errors.New("and requires a expression")
-		}		
+		}
 		rulers := make([]Ruler, 0)
 		for n := node.next; n != nil; n = n.next {
 			newRuler, err := eval(n, e)
@@ -27,7 +27,7 @@ func eval(node *astNode, e *env) (Ruler, error) {
 	case orToken:
 		if node.next == nil {
 			return nil, errors.New("or requires a expression")
-		}			
+		}
 		rulers := make([]Ruler, 0)
 		for n := node.next; n != nil; n = n.next {
 			newRuler, err := eval(n, e)
@@ -38,9 +38,9 @@ func eval(node *astNode, e *env) (Ruler, error) {
 		}
 		return OrRuler(rulers), nil
 	case atLeastToken:
-		if node.next == nil || node.next.next == nil{
+		if node.next == nil || node.next.next == nil {
 			return nil, errors.New("atLeast requires a symbol and rule expressions")
-		}		
+		}
 		rulers := make([]Ruler, 0)
 		n, err := strconv.Atoi(node.next.token.buf)
 		if err != nil {
@@ -55,9 +55,9 @@ func eval(node *astNode, e *env) (Ruler, error) {
 		}
 		return NewAtLeastNRuler(rulers, n), nil
 	case atMostToken:
-		if node.next == nil || node.next.next == nil{
+		if node.next == nil || node.next.next == nil {
 			return nil, errors.New("atMost requires a symbol and rule expressions")
-		}			
+		}
 		rulers := make([]Ruler, 0)
 		n, err := strconv.Atoi(node.next.token.buf)
 		if err != nil {
@@ -71,59 +71,78 @@ func eval(node *astNode, e *env) (Ruler, error) {
 			rulers = append(rulers, newRuler)
 		}
 		return NewAtMostNRuler(rulers, n), nil
-	// 括号		
+	// 括号
 	case leftParenthesisToken:
 		return eval(node.child, e)
-	// 否运算	
+	// 否运算
 	case notToken:
 		newRuler, err := eval(node.next, e)
-		if err != nil{
+		if err != nil {
 			return nil, err
 		}
 		return Negate(newRuler), nil
 	case letToken:
-		if node.next == nil || node.next.next == nil{
+		if node.next == nil || node.next.next == nil {
 			return nil, errors.New("let requires a symbol and a rule expression")
 		}
 		newRuler, err := eval(node.next.next, e)
-		if err != nil{
+		if err != nil {
 			return nil, err
 		}
 		e.set(node.next.token.buf, newRuler)
 		return newRuler, nil
 	case unitRuleToken:
 		return parseUnitRule(node.token.buf)
-	// symbol token	
+	// symbol token
 	default:
-		return e.get(node.token.buf), nil	
+		return e.get(node.token.buf), nil
 	}
 }
 
-type Evaluator struct{
+type ExpectResult struct {
+	RuleName  string `json:"rule_name"`
+	Ok        bool   `json:"ok"`
+	IsPending bool   `json:"isPending"`
+}
+
+type Evaluator struct {
 	e *env
 }
 
-func (evaler *Evaluator) Eval(expressions string)(Ruler, error){
+func (evaler *Evaluator) Eval(expressions string) (Ruler, error) {
 	expressions = commentPattern.ReplaceAllString(expressions, "")
 	var ruler Ruler
 	var err error
-	for _, expr := range strings.Split(expressions, ";"){
-		if strings.TrimSpace(expr) == ""{
+	for _, expr := range strings.Split(expressions, ";") {
+		if strings.TrimSpace(expr) == "" {
 			continue
 		}
 		node, err := buildAST(tokenize(expr))
-		if err != nil{
+		if err != nil {
 			return nil, errors.New("expression " + expr + " parse fail")
 		}
 		ruler, err = eval(node, evaler.e)
-		if err != nil{
+		if err != nil {
 			return nil, err
 		}
 	}
 	return ruler, err
 }
 
-func NewEvaluator() *Evaluator{
+func (evaler *Evaluator) Expect(c Context) map[string]*ExpectResult {
+	results := make(map[string]*ExpectResult)
+	for n, r := range evaler.e.vars {
+		result, err := r.Rule(c)
+		results[n] = &ExpectResult{
+			RuleName:  n,
+			Ok:        result,
+			IsPending: err != nil,
+		}
+	}
+	return results
+}
+
+func NewEvaluator() *Evaluator {
 	return &Evaluator{
 		e: newEnv(),
 	}
--- eval_test.go
+++ eval_test.go
@@ -1,16 +1,17 @@
 package rule
 
-import(
-	"testing"
-	"github.com/stretchr/testify/assert"	
+import (
 	"io/ioutil"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 // 心包经<=-9 或者任意两条经络>=8或<=8
 func TestEval(t *testing.T) {
 	evaultor := NewEvaluator()
 	evaultor.Eval(`(let rs1rule1 'c0>=0')`)
-	rs1rule1, err := evaultor.Eval("((rs1rule1))")	
+	rs1rule1, err := evaultor.Eval("((rs1rule1))")
 	ctx := NewContext(map[string]interface{}{
 		"c0": -1,
 	})
@@ -21,22 +22,22 @@ func TestEval(t *testing.T) {
 }
 
 // 测试变量组合
-func TestEvalComposite(t *testing.T){
+func TestEvalComposite(t *testing.T) {
 	evaultor := NewEvaluator()
-	evaultor.Eval(`(let r1 'c0 <= -9')`);
+	evaultor.Eval(`(let r1 'c0 <= -9')`)
 	evaultor.Eval(`(let r2 
 			(atLeast 2 'c0>=8' 'c1>=8' 'c2>=8' 'c3>=8' 'c4>=8' 'c5>=8' 'c6>=8' 'c7>=8')
 		)`)
 	evaultor.Eval(`(let r3 
 			(atLeast 2 'c0<=-8' 'c1<=-8' 'c2<=-8' 'c3<=-8' 'c4<=-8' 'c5<=-8' 'c6<=-8' 'c7<=-8')
-		)`)		
+		)`)
 	evaultor.Eval(`(
 			let r4
 			(or r1 r2 r3)
-		)`)	
-	r1, _ := evaultor.Eval("((r1))")	
-	r2, _ := evaultor.Eval("r2")	
-	r3, _ := evaultor.Eval("r3")	
+		)`)
+	r1, _ := evaultor.Eval("((r1))")
+	r2, _ := evaultor.Eval("r2")
+	r3, _ := evaultor.Eval("r3")
 	r4, _ := evaultor.Eval("(r4)")
 	assert.NotNil(t, r1)
 	assert.NotNil(t, r2)
@@ -52,15 +53,15 @@ func TestEvalComposite(t *testing.T){
 		"c6": 0,
 		"c7": 0,
 	})
-	result, err := r4.Rule(ctx)	
+	result, err := r4.Rule(ctx)
 	assert.NoError(t, err)
 	assert.True(t, result)
 }
 
 // 测试变量组合
-func TestEvalComposite2(t *testing.T){
+func TestEvalComposite2(t *testing.T) {
 	evaultor := NewEvaluator()
-	evaultor.Eval(`(let r1 'c0 <= -9')`);
+	evaultor.Eval(`(let r1 'c0 <= -9')`)
 	evaultor.Eval(`(let r1 
 			(or r1 (atLeast 2 'c0>=8' 'c1>=8' 'c2>=8' 'c3>=8' 'c4>=8' 'c5>=8' 'c6>=8' 'c7>=8'))
 		)`)
@@ -68,8 +69,8 @@ func TestEvalComposite2(t *testing.T){
 			(or r1 
 				(atLeast 2 'c0<=-8' 'c1<=-8' 'c2<=-8' 'c3<=-8' 'c4<=-8' 'c5<=-8' 'c6<=-8' 'c7<=-8')
 			)
-		)`)		
-	r1, _ := evaultor.Eval("((r1))")	
+		)`)
+	r1, _ := evaultor.Eval("((r1))")
 	ctx := NewContext(map[string]interface{}{
 		"c0": -9,
 		"c1": 0,
@@ -80,15 +81,15 @@ func TestEvalComposite2(t *testing.T){
 		"c6": 0,
 		"c7": 0,
 	})
-	result, err := r1.Rule(ctx)	
+	result, err := r1.Rule(ctx)
 	assert.NoError(t, err)
 	assert.True(t, result)
 }
 
 // 测试 rs1rule1 成功
-func Testrs1rule1success(t *testing.T){
+func Testrs1rule1success(t *testing.T) {
 	evaultor := NewEvaluator()
-	evaultor.Eval(`(let rs1rule1 'c0 <= -9')`);
+	evaultor.Eval(`(let rs1rule1 'c0 <= -9')`)
 	evaultor.Eval(`(let rs1rule1 
 			(or rs1rule1 (atLeast 2 'c0>=8' 'c1>=8' 'c2>=8' 'c3>=8' 'c4>=8' 'c5>=8' 'c6>=8' 'c7>=8'))
 		)`)
@@ -96,31 +97,31 @@ func Testrs1rule1success(t *testing.T){
 			(or rs1rule1 
 				(atLeast 2 'c0<=-8' 'c1<=-8' 'c2<=-8' 'c3<=-8' 'c4<=-8' 'c5<=-8' 'c6<=-8' 'c7<=-8')
 			)
-		)`)	
+		)`)
 	evaultor.Eval(`(
 		let rs1rule1 (and rs1rule1 'isSportOrDrunk=1' )
-	)`)		
-	rs1rule1, _ := evaultor.Eval("((rs1rule1))")	
+	)`)
+	rs1rule1, _ := evaultor.Eval("((rs1rule1))")
 	ctx := NewContext(map[string]interface{}{
-		"c0": 10,
-		"c1": 0,
-		"c2": 0,
-		"c3": 0,
-		"c4": 0,
-		"c5": 0,
-		"c6": 0,
-		"c7": 0,
+		"c0":             10,
+		"c1":             0,
+		"c2":             0,
+		"c3":             0,
+		"c4":             0,
+		"c5":             0,
+		"c6":             0,
+		"c7":             0,
 		"isSportOrDrunk": 1,
 	})
-	result, err := rs1rule1.Rule(ctx)	
+	result, err := rs1rule1.Rule(ctx)
 	assert.NoError(t, err)
 	assert.True(t, result)
 }
 
 // 测试 rs1rule1 失败
-func Testrs1rule1fail(t *testing.T){
+func Testrs1rule1fail(t *testing.T) {
 	evaultor := NewEvaluator()
-	evaultor.Eval(`(let rs1rule1 'c0 <= -9')`);
+	evaultor.Eval(`(let rs1rule1 'c0 <= -9')`)
 	evaultor.Eval(`(let rs1rule1 
 			(or rs1rule1 (atLeast 2 'c0>=8' 'c1>=8' 'c2>=8' 'c3>=8' 'c4>=8' 'c5>=8' 'c6>=8' 'c7>=8'))
 		)`)
@@ -128,29 +129,29 @@ func Testrs1rule1fail(t *testing.T){
 			(or rs1rule1 
 				(atLeast 2 'c0<=-8' 'c1<=-8' 'c2<=-8' 'c3<=-8' 'c4<=-8' 'c5<=-8' 'c6<=-8' 'c7<=-8')
 			)
-		)`)		
+		)`)
 	evaultor.Eval(`(
 		let rs1rule1 (and rs1rule 'isSportOrDrunk=1')
 	`)
-	rs1rule1, _ := evaultor.Eval("((rs1rule1))")	
+	rs1rule1, _ := evaultor.Eval("((rs1rule1))")
 	ctx := NewContext(map[string]interface{}{
-		"c0": 0,
-		"c1": 0,
-		"c2": 0,
-		"c3": 0,
-		"c4": 0,
-		"c5": 0,
-		"c6": 0,
-		"c7": 0,
+		"c0":             0,
+		"c1":             0,
+		"c2":             0,
+		"c3":             0,
+		"c4":             0,
+		"c5":             0,
+		"c6":             0,
+		"c7":             0,
 		"isSportOrDrunk": 0,
 	})
-	result, err := rs1rule1.Rule(ctx)	
+	result, err := rs1rule1.Rule(ctx)
 	assert.NoError(t, err)
 	assert.False(t, result)
 }
 
 // 测试 rs2rule1 成功
-func Testrs2rule1success(t *testing.T){
+func Testrs2rule1success(t *testing.T) {
 	evaultor := NewEvaluator()
 	evaultor.Eval(`(let rs2rule1 (
 			and 'c4 >= 20' (
@@ -161,27 +162,27 @@ func Testrs2rule1success(t *testing.T){
 	evaultor.Eval(`(let rs2rule1 (
 			and rs2rule1 'isCold=1'
 		)
-	)`)	
-	rs2rule1, _ := evaultor.Eval("((rs1rule1))")	
+	)`)
+	rs2rule1, _ := evaultor.Eval("((rs1rule1))")
 	ctx := NewContext(map[string]interface{}{
-		"c0": 0,
-		"c1": 0,
-		"c2": 0,
-		"c3": 0,
-		"c4": 20,
-		"c5": 0,
-		"c6": 0,
-		"c7": 0,
+		"c0":        0,
+		"c1":        0,
+		"c2":        0,
+		"c3":        0,
+		"c4":        20,
+		"c5":        0,
+		"c6":        0,
+		"c7":        0,
 		"heartRate": 90,
-		"isCold": 1,
+		"isCold":    1,
 	})
-	result, err := rs2rule1.Rule(ctx)	
+	result, err := rs2rule1.Rule(ctx)
 	assert.NoError(t, err)
 	assert.True(t, result)
 }
 
 // 测试 rs2rule1 失败
-func Testrs2rule1fail(t *testing.T){
+func Testrs2rule1fail(t *testing.T) {
 	evaultor := NewEvaluator()
 	evaultor.Eval(`(let rs2rule1 (
 			and 'c4 >= 20' (
@@ -192,44 +193,43 @@ func Testrs2rule1fail(t *testing.T){
 	evaultor.Eval(`(let rs2rule1 (
 			and rs2rule1 'isCold=1'
 		)
-	)`)		
-	rs2rule1, _ := evaultor.Eval("((rs1rule1))")	
+	)`)
+	rs2rule1, _ := evaultor.Eval("((rs1rule1))")
 	ctx := NewContext(map[string]interface{}{
-		"c0": 0,
-		"c1": 0,
-		"c2": 0,
-		"c3": 0,
-		"c4": 20,
-		"c5": 0,
-		"c6": 0,
-		"c7": 0,
+		"c0":        0,
+		"c1":        0,
+		"c2":        0,
+		"c3":        0,
+		"c4":        20,
+		"c5":        0,
+		"c6":        0,
+		"c7":        0,
 		"heartRate": 0,
-		"isCold":0,
+		"isCold":    0,
 	})
-	result, err := rs2rule1.Rule(ctx)	
+	result, err := rs2rule1.Rule(ctx)
 	assert.NoError(t, err)
 	assert.False(t, result)
 }
 
-func TestFile(t *testing.T){
+func TestFile(t *testing.T) {
 	data, err := ioutil.ReadFile("./testdata/rules.txt")
 	assert.NoError(t, err)
 	evaultor := NewEvaluator()
-	_, err = evaultor.Eval(string(data))	
+	_, err = evaultor.Eval(string(data))
 	assert.NoError(t, err)
-	rs1rule1, err := evaultor.Eval("rs1rule1")
 	ctx := NewContext(map[string]interface{}{
-		"c0": -10,
-		"c1": 0,
-		"c2": 0,
-		"c3": 0,
-		"c4": 0,
-		"c5": 0,
-		"c6": 0,
-		"c7": 0,
+		"c0":             -8,
+		"c1":             0,
+		"c2":             0,
+		"c3":             0,
+		"c4":             0,
+		"c5":             0,
+		"c6":             0,
+		"c7":             0,
 		"isSportOrDrunk": 1,
 	})
-	result, err := rs1rule1.Rule(ctx)	
+	results := evaultor.Expect(ctx)
 	assert.NoError(t, err)
-	assert.True(t, result)	
+	assert.False(t, results["rs1rule1"].Ok)
 }
